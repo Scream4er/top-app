8456ae3e35f8f2c1f780eb085300410844a81be5
ScreamKO <Chaykov.Artem1@gmail.com>
2023-01-03 17:58:53 +0100

    Create css for project
    use Grids

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,12 +1,15 @@
 import styles from "./Footer.module.css";
 import { FooterProps } from "./Footer.props";
 import cn from "classnames";
+import { format } from "date-fns"
 
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
     return (
-        <div {...props}>
-            Footer
-        </div>
+        <footer className={cn(className, styles.footer)} {...props}>
+            <div>ScreamKO © 2022 - {format(new Date(), 'yyyy')} All rights reserved</div>
+            <a href="#" target="_blank">User Agreement</a>
+            <a href="#" target="_blank">Privacy Policy</a>
+        </footer>
     );
 };
